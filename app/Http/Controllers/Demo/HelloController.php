c42855e9b544da4b040d3666ae1c7c6dd3a1b67d
xinchin@gmail.com <xinchin@gmail.com>
2020-06-14 22:16:22 +0800

    dump user infomation

--- app/Http/Controllers/Demo/HelloController.php
+++ app/Http/Controllers/Demo/HelloController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers\Demo;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class HelloController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     //
     public function index(){
+        dump(Auth::user());
+        dump(Auth::id());
         return 'hello';
     }
 }
